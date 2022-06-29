f3bfacf97a962b07280cfb6c139c498bb6e5230c
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-06-29 15:11:45 -0400

    Add getDogsById

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -43,20 +43,25 @@ module.exports = {
     getDogsById: async (req,res) => {
         const { id } = req.params
         const { data } = await axios.get(
-            `https://api.thedogapi.com/v1/breeds/search?q=${name}`,{
+            `https://api.thedogapi.com/v1/breeds`,{
             headers: {'x-api-key': process.env.API_KEY}
-        })
-        .catch(error => res.status(400).json(error))
-        const dog = data.find(dog => dog.id === parseInt(id))
-        // dog = {
-        //     dog_id: dog.id,
-        //     name: dog.name,
-        //     height: dog.height.metric,
-        //     weight: dog.weight.metric,
-        //     // img: b.image.url,
-        //     temperament: dog.temperament,
-        //     life_span: dog.life_span
+        }).catch(error => res.status(400).json(error))
+        // try {
+        //     id = parseInt(id)
+        // } catch (error) {
+        //     res.status(400).json(error)
         // }
+        const dogFound = data.find(dog => dog.id === parseInt(id))
+        if (dogFound === undefined) return res.status(404).json({error: "The Dog doesn't exist"})
+        const dog = {
+            dog_id: dogFound.id,
+            name: dogFound.name,
+            height: dogFound.height.metric,
+            weight: dogFound.weight.metric,
+            // img: b.image.url,
+            temperament: dogFound.temperament,
+            life_span: dogFound.life_span
+        }
         return res.json(dog)
     }
 }
