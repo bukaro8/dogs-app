5af8eeeeb63c537580c66299ea72b01ec4311c40
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-06-29 14:25:54 -0400

    Add getDogsById

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -27,7 +27,7 @@ module.exports = {
             headers: {'x-api-key': process.env.API_KEY}
         })
         .catch(error => res.status(400).json(error))
-        const breeds = data.map(b => {
+        const dog = data.map(b => {
             return {
                 dog_id: b.id,
                 name: b.name,
@@ -38,6 +38,25 @@ module.exports = {
                 life_span: b.life_span
             }
         })
-        return res.json(breeds)
+        return res.json(dog)
+    },
+    getDogsById: async (req,res) => {
+        const { id } = req.params
+        const { data } = await axios.get(
+            `https://api.thedogapi.com/v1/breeds/search?q=${name}`,{
+            headers: {'x-api-key': process.env.API_KEY}
+        })
+        .catch(error => res.status(400).json(error))
+        const dog = data.find(dog => dog.id === parseInt(id))
+        dog = {
+            dog_id: dog.id,
+            name: dog.name,
+            height: dog.height.metric,
+            weight: dog.weight.metric,
+            // img: b.image.url,
+            temperament: dog.temperament,
+            life_span: dog.life_span
+        }
+        return res.json(dog)
     }
 }
