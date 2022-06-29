1dba35039a20211ecf7486f6d4806c5cf91e4930
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-06-29 14:13:22 -0400

    Add 'dogs' routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,18 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const { getDogs, getDogsByName } = require('../controllers/index')
 
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.get('/dogs',(req,res) => {
+    if (req.query.name){
+        return getDogsByName(req,res)
+    }
+    return getDogs(req,res)
+})
 
 module.exports = router;
