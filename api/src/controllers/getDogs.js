9c3b0069b38d04de56e8f13fa5e9b933112b0bad
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-07-02 19:40:28 -0400

    Delete unnecesary comments

--- api/src/controllers/getDogs.js
+++ api/src/controllers/getDogs.js
@@ -1,9 +1,14 @@
 
 const instance = require('./includes/axios')
+const { Dogs, Temperaments } = require('../db')
 
 exports.getDogs = async (req,res) => {
     const { data } = await instance.get('/breeds')
     .catch(error => res.status(400).json(error))
+    // const dogs = await Dogs.findAll({ include: Temperaments }).catch(error => console.log(error))
+    // if (dogs){
+    //     console.log(JSON.stringify(dogs))
+    // }
     const breeds = data.map(b => {
         return {
             dog_id: b.id,
