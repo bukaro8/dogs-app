30b7e7bd1d0a0e765f04394d394028b259c986dd
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-07-02 20:03:12 -0400

    Change validation rule

--- api/src/models/Dogs.js
+++ api/src/models/Dogs.js
@@ -19,8 +19,8 @@ module.exports = (sequelize) => {
       allowNull: false,
       unique: true,
       validate: {
-        is: /\p{L}/gi, // valida que sean solo letras
-        msg: "Nombre de raza no válido"
+        is: /^[ a-zA-ZÀ-ÿ\u00f1\u00d1]*$/g,// /\p{L}/gi, // valida que sean solo letras y espacios
+        // msg: "Nombre de raza no válido"
       }
     },
     weight: {
