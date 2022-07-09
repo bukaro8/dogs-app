08bca3f1873e9e9ae6053440b62ebaa6411d9796
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-07-09 17:00:54 -0400

    Mix BD data to API data

--- api/src/controllers/getDogs.js
+++ api/src/controllers/getDogs.js
@@ -5,11 +5,7 @@ const { Dogs, Temperaments } = require('../db')
 exports.getDogs = async (req,res) => {
     const { data } = await instance.get('/breeds')
     .catch(error => res.status(400).json(error))
-    // const dogs = await Dogs.findAll({ include: Temperaments }).catch(error => console.log(error))
-    // if (dogs){
-    //     console.log(JSON.stringify(dogs))
-    // }
-    const breeds = data.map(b => {
+    let breeds = data.map((b,idx) => {
         return {
             dog_id: b.id,
             name: b.name,
@@ -20,5 +16,18 @@ exports.getDogs = async (req,res) => {
             life_span: b.life_span
         }
     })
+    const dogs = await Dogs.findAll({ include: [Temperaments] }).catch(error => console.log(error))
+    if (dogs){
+        dogs.forEach(dog => {
+            breeds.push({
+                dog_id:dog.dog_id,
+                name:dog.name,
+                height:dog.height,
+                weight:dog.weight,
+                life_span:dog.life_span,
+                temperament:dog.temperaments.map(t => t.name).join(", ")
+            })
+        });
+    }
     return res.json(breeds)
 }
