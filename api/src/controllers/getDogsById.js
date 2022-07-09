2663b02170eb1c669b6fcb35109e3a4b87fc30dd
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-07-09 18:59:16 -0400

    Mix DB data with API data

--- api/src/controllers/getDogsById.js
+++ api/src/controllers/getDogsById.js
@@ -1,20 +1,34 @@
 
 const instance = require('./includes/axios')
+const { Dogs, Temperaments } = require('../db')
 
 exports.getDogsById = async (req,res) => {
     const { id } = req.params
-    const { data } = await instance.get('/breeds')
-    .catch(error => res.status(400).json(error))
-    const dogFound = data.find(dog => dog.id === parseInt(id))
-    if (!dogFound) return res.status(404).json({error: "The Dog doesn't exist"})
-    const dog = {
-        dog_id: dogFound.id,
-        name: dogFound.name,
-        height: dogFound.height.metric,
-        weight: dogFound.weight.metric,
-        // img: b.image.url,
-        temperament: dogFound.temperament,
-        life_span: dogFound.life_span
+    let dog;
+    if (isNaN(id)){
+        const dogFound = await Dogs.findByPk(id, { include: [Temperaments] })
+        if(!dogFound) return res.status(404).json({error: "The Dog doesn't exist"})
+        dog = {
+            dog_id: dogFound.dog_id,
+            name: dogFound.name,
+            height: dogFound.height,
+            weight: dogFound.weight,
+            temperament: dogFound.temperaments.map(t => t.name).join(", "),
+            life_span: dogFound.life_span
+        }
+    } else {
+        const { data } = await instance.get('/breeds')
+        .catch(error => res.status(500).json(error))
+        const dogFound = data.find(dog => dog.id === parseInt(id))
+        if (!dogFound) return res.status(404).json({error: "The Dog doesn't exist"})
+        dog = {
+            dog_id: dogFound.id,
+            name: dogFound.name,
+            height: dogFound.height.metric,
+            weight: dogFound.weight.metric,
+            temperament: dogFound.temperament,
+            life_span: dogFound.life_span
+        }
     }
     return res.json(dog)
 }
